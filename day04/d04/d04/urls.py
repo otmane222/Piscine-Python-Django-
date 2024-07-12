01c1410097d65f23b3cea94e2f05c497e895b287
Otmane Aboulghit <oaboulgh@e3r8p12.1337.ma>
2024-07-13 00:45:10 +0100

    commit for merge

--- day04/d04/d04/urls.py
+++ day04/d04/d04/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('ex00/', include('ex00.urls')),
     path('admin/', admin.site.urls),
+    path('ex00/', include('ex00.urls')),
+    path('ex01/', include('ex01.urls')),
 ]
