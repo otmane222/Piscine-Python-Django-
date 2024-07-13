de082954daeed21cd88d9c8a66c50c84aedf3a13
Otmane Aboulghit <oaboulgh@e3r8p14.1337.ma>
2024-07-13 21:30:46 +0100

    day05 in progress

--- day05/ex00/models.py
+++ day05/ex00/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class tablo(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField()
+class Movie(models.Model):
+    title = models.CharField(max_length=64, null=False, unique=True)
+    episode_nb = models.AutoField(primary_key=True)
+    opening_crawl = models.TextField(null=True)
+    director = models.CharField(max_length=32, null=False)
+    producer = models.CharField(max_length=128, null=False)
+    release_date = models.DateField(null=False)
+
+    class Meta:
+        db_table = 'ex00_movies'
